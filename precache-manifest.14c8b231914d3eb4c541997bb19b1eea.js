9dea939434cdc0893ce5718be62d45ca4ee7f312
unknown <jesusvaz@gmail.com>
2020-09-23 18:12:32 -0600

    Updates

---
+++ precache-manifest.14c8b231914d3eb4c541997bb19b1eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68eb1233006b9f59ce6b8daf08aec99",
+    "url": "/niagara/index.html"
+  },
+  {
+    "revision": "7c5c71b5dd6a96897aff",
+    "url": "/niagara/static/css/main.4ac8c28c.chunk.css"
+  },
+  {
+    "revision": "01b89ebba9538091a382",
+    "url": "/niagara/static/js/2.20a8f0f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/niagara/static/js/2.20a8f0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5c71b5dd6a96897aff",
+    "url": "/niagara/static/js/main.45fe11a5.chunk.js"
+  },
+  {
+    "revision": "75a2b25ce13d37225fd1",
+    "url": "/niagara/static/js/runtime-main.5555af6e.js"
+  },
+  {
+    "revision": "fcd44f6b788ed3a76d815d54a7dd6114",
+    "url": "/niagara/static/media/Logo.fcd44f6b.png"
+  }
+]);
